37afeeb6da7b820fd549e1b1b80ee00ffe9081db
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-14 16:09:11 +0800

    Extract boolean to variables; use ternary for return

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,10 +2,8 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     public String renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
-            return "IE on Mac?";
-        }
-        return "banner";
+        boolean platformIsMac = platform.toUpperCase().indexOf("MAC") > -1;
+        boolean browserIsIe = browser.toUpperCase().indexOf("IE") > -1;
+        return platformIsMac && browserIsIe ? "IE on Mac?" : "banner";
     }
 }
