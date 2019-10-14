f383bace4b124b80505e9b7d33579f1e874fa188
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-14 16:09:55 +0800

    Use contains instead of indexOf > -1

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,8 +2,8 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     public String renderBanner(String platform, String browser) {
-        boolean platformIsMac = platform.toUpperCase().indexOf("MAC") > -1;
-        boolean browserIsIe = browser.toUpperCase().indexOf("IE") > -1;
+        boolean platformIsMac = platform.toUpperCase().contains("MAC");
+        boolean browserIsIe = browser.toUpperCase().contains("IE");
         return platformIsMac && browserIsIe ? "IE on Mac?" : "banner";
     }
 }
