0e07d75c7d284f5d78d2607b9934fc7675e67d8f
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-14 15:52:57 +0800

    Extract discount and shippingCost to variables

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -1,10 +1,12 @@
 package com.tws.refactoring.extract_variable;
 
 public class PriceCalculator {
+
     public double getPrice(int quantity, int itemPrice) {
-        // Price consists of: base price - discount + shipping cost
-        return quantity * itemPrice -
-                Math.max(0, quantity - 500) * itemPrice * 0.05 +
-                Math.min(quantity * itemPrice * 0.1, 100.0);
+        double discount = Math.max(0, quantity - 500) * itemPrice * 0.05;
+        double shippingCost = Math.min(quantity * itemPrice * 0.1, 100.0);
+
+        return quantity * itemPrice - discount + shippingCost;
     }
+
 }
